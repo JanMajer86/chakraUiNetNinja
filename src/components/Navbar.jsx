ccbd945d29b72d5a565c32708d9423736530ea30
JanMajer86 <majer.jan.1986@gmail.com>
2025-04-16 13:45:03 +0200

    components, styles, flex layouts

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import {
+	Box,
+	Button,
+	Flex,
+	Heading,
+	HStack,
+	Spacer,
+	Text,
+} from "@chakra-ui/react";
+
+export default function Navbar() {
+	return (
+		// <Flex as="nav" p="10px" alignItems="center" gap="10px">
+		<Flex as="nav" p="10px" alignItems="center">
+			<Heading as="h1">Dojo Tasks</Heading>
+			<Spacer />
+			<HStack gap="20px">
+				<Box bg="gray.200" p="10px">
+					M
+				</Box>
+				<Text>mario@netninja.dev</Text>
+				<Button colorPalette="purple">Logout</Button>
+			</HStack>
+		</Flex>
+
+		// <Flex bg='gray.200' justify='space-between' wrap='wrap' gap='2'>
+		// 	<Box w='150px' h='50px' bg='red'>
+		// 		1
+		// 	</Box>
+		// 	<Box w='150px' h='50px' bg='blue'>
+		// 		2
+		// 	</Box>
+		// 	<Box w='150px' h='50px' bg='green' flexGrow='1'>
+		// 		3
+		// 	</Box>
+		// 	<Box w='150px' h='50px' bg='yellow' flexGrow='2'>
+		// 		4
+		// 	</Box>
+		// </Flex>
+	);
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,37 @@
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+	const boxStyles = {
+		p: "10px",
+		bg: "purple.400",
+		color: "white",
+		m: "10px",
+		textAlign: "center",
+		filter: "blur(2px)",
+		_hover: {
+			color: "black",
+			bg: "blue.200",
+			filter: "none",
+		},
+	};
+
+	return (
+		<Container as="section" maxWidth="2xl" py="20px">
+			<Heading my="30px" p="10px">
+				Chakra UI Components
+			</Heading>
+			<Text ml="30px">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, earum.
+			</Text>
+			<Text marginLeft="30px" color="blue.300" fontWeight="bold">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, earum.
+			</Text>
+
+			<Box my="30px" p="20px" bg="orange">
+				<Text color="white">This is a box</Text>
+			</Box>
+
+			<Box css={boxStyles}>Hello, Ninjas!</Box>
+		</Container>
+	);
 }
